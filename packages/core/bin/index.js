0c730ac55c2a46360a87bb24b8636197182fea77
hotmooner <37256998+hotmooner@users.noreply.github.com>
2021-10-07 13:21:36 +0800

    init

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -1,9 +1,9 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
+console.log(process.argv.slice(2))
 
-const importLocal = require('import-local');
-
-if (importLocal(__filename)) {
-  require('npmlog').info('cli', '正在使用 imooc-cli 本地版本');
-} else {
-  require('../lib')(process.argv.slice(2));
-}
+// const importLocal = require('import-local');
+// if (importLocal(__filename)) {
+//   require('npmlog').info('cli', '正在使用 imooc-cli 本地版本');
+// } else {
+//   require('../lib')(process.argv.slice(2));
+// }
